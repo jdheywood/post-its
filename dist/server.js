aea91c598e21ebd964f1d5fab412e99b75a19dd8
James Heywood <jdheywood@gmail.com>
2016-11-21 22:20:40 +0000

    Removed dist from ignore as plan to deploy the built app from here and therefore need in source control

--- dist/server.js
+++ dist/server.js
@@ -1,10 +1,12 @@
 'use strict'
 
 let express = require('express')
-let path = require('path')
 let serveStatic = require('serve-static')
+
 let app = express()
-app.use(serveStatic(path.join(__dirname, 'dist')))
+app.use(serveStatic(__dirname))
+
 let port = process.env.PORT || 5000
 app.listen(port)
+
 console.log('server started ' + port)
